dfa64340225ba55dbcd0bc6e5862a33525f7df80
dimkafeed <banshchikov.2021@bk.ru>
2025-01-09 17:01:35 +0500

    Добавьте файлы проекта.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "Window");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
+}
